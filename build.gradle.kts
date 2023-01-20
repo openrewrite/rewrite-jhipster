d8a90d98e415dd5836df619a7b8ec0228bececb0
Sam Snyder <sam@moderne.io>
2023-01-19 22:47:57 -0800

    Update for compatibility with rewrite 7.35.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,89 +1,10 @@
-import nebula.plugin.contacts.Contact
-import nebula.plugin.contacts.ContactsExtension
-import nebula.plugin.release.NetflixOssStrategies.SNAPSHOT
-import nebula.plugin.release.git.base.ReleasePluginExtension
-import nl.javadude.gradle.plugins.license.LicenseExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.*
-
 plugins {
-    `java-library`
-    `maven-publish`
-    signing
-
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("nebula.maven-resolved-dependencies") version "17.3.2"
-    id("nebula.release") version "15.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-
-    id("com.github.hierynomus.license") version "0.16.1"
-    id("com.github.jk1.dependency-license-report") version "1.16"
-    id("org.owasp.dependencycheck") version "7.0.4.1"
-
-    id("nebula.maven-publish") version "17.3.2"
-    id("nebula.contacts") version "5.1.0"
-    id("nebula.info") version "9.3.0"
-
-    id("nebula.javadoc-jar") version "17.3.2"
-    id("nebula.source-jar") version "17.3.2"
-    id("nebula.maven-apache-license") version "17.3.2"
-
-    id("org.openrewrite.rewrite") version "latest.release"
-}
-
-rewrite {
-    activeRecipe("org.openrewrite.java.format.AutoFormat", "org.openrewrite.java.cleanup.Cleanup")
-}
-
-apply(plugin = "nebula.publish-verification")
-
-configure<ReleasePluginExtension> {
-    defaultVersionStrategy = SNAPSHOT(project)
-}
-
-dependencyCheck {
-    analyzers.assemblyEnabled = false
-    suppressionFile = "suppressions.xml"
-    failBuildOnCVSS = 9.0F
+    id("org.openrewrite.build.recipe-library") version "latest.release"
 }
 
 group = "org.openrewrite.recipe"
 description =
     "Collection of Rewrite Recipes pertaining to the JHipster web application & microservice development platform"
-
-repositories {
-    if (!project.hasProperty("releasing")) {
-        mavenLocal()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-    }
-    mavenCentral()
-}
-
-nexusPublishing {
-    repositories {
-        sonatype()
-    }
-}
-
-signing {
-    setRequired({
-        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
-    })
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications["nebula"])
-}
-
-
-configurations.all {
-    resolutionStrategy {
-        cacheChangingModulesFor(0, TimeUnit.SECONDS)
-        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
-    }
-}
 
 val rewriteVersion = if (project.hasProperty("releasing")) {
     "latest.release"
@@ -91,19 +12,16 @@ val rewriteVersion = if (project.hasProperty("releasing")) {
     "latest.integration"
 }
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(platform("org.openrewrite:rewrite-bom:${rewriteVersion}"))
     implementation("org.openrewrite:rewrite-java")
     implementation("org.openrewrite:rewrite-maven")
-    runtimeOnly("org.openrewrite:rewrite-java-11")
+    runtimeOnly("org.openrewrite:rewrite-java-17")
     runtimeOnly("com.fasterxml.jackson.core:jackson-core:latest.release")
 
     compileOnly("org.projectlombok:lombok:latest.release")
     annotationProcessor("org.projectlombok:lombok:latest.release")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.openrewrite:rewrite-java-11")
+    testImplementation("org.openrewrite:rewrite-java-17")
     testImplementation("org.openrewrite:rewrite-java-tck")
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.assertj:assertj-core:latest.release")
@@ -112,79 +30,4 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
     testRuntimeOnly("org.apache.commons:commons-lang3:3.11")
     testRuntimeOnly("commons-lang:commons-lang:2.6")
-}
-
-tasks.withType(KotlinCompile::class.java).configureEach {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-    jvmArgs = listOf("-Xmx1g", "-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
-}
-
-tasks.named<JavaCompile>("compileJava") {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-    targetCompatibility = JavaVersion.VERSION_1_8.toString()
-
-    options.isFork = true
-    options.compilerArgs.addAll(listOf("--release", "8"))
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
-}
-
-tasks.withType<Javadoc> {
-    // assertTrue(boolean condition) -> assertThat(condition).isTrue()
-    // warning - invalid usage of tag >
-    // see also: https://blog.joda.org/2014/02/turning-off-doclint-in-jdk-8-javadoc.html
-    (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
-}
-
-configure<ContactsExtension> {
-    val j = Contact("jkschneider@gmail.com")
-    j.moniker("Jonathan Schneider")
-
-    people["jkschneider@gmail.com"] = j
-}
-
-configure<LicenseExtension> {
-    ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
-    skipExistingHeaders = true
-    header = project.rootProject.file("gradle/licenseHeader.txt")
-    mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
-    strictCheck = true
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-
-            pom.withXml {
-                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
-                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
-                        var i = 0
-                        var length = dependencyList.length
-                        while (i < length) {
-                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
-                                if ((dependency.getElementsByTagName("scope")
-                                        .item(0) as org.w3c.dom.Element).textContent == "provided"
-                                ) {
-                                    dependencies.removeChild(dependency)
-                                    i--
-                                    length--
-                                }
-                            }
-                            i++
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
